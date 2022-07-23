b0e3688c814ba0207d609573f610dc5dadd52d09
Daniel Król <danielkrol90@gmail.com>
2022-07-23 16:52:51 +0200

    progressing media responsive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("working here")
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const navbar = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        navbar.classList.add('active');
+    });
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        navbar.classList.remove('active');
+    });
+}
