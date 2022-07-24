4000568dcf131723743d110ff40a343e4e17a392
Daniel Król <danielkrol90@gmail.com>
2022-07-24 20:27:49 +0200

    somehow media for product-detail done

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const navbar = document.getElementById('navbar');
 
+// MOBILE MENU 
 if (bar) {
     bar.addEventListener('click', () => {
         navbar.classList.add('active');
@@ -13,3 +14,21 @@ if (close) {
         navbar.classList.remove('active');
     });
 }
+
+// PRODUCT DETAILS PAGE
+let mainImage = document.getElementById('item-image-main');
+let subImage = document.getElementsByClassName('item-image-sub');
+
+
+subImage[0].onclick = function() {
+    mainImage.src = subImage[0].src;
+};
+subImage[1].onclick = function() {
+    mainImage.src = subImage[1].src;
+};
+subImage[2].onclick = function() {
+    mainImage.src = subImage[2].src;
+};
+subImage[3].onclick = function() {
+    mainImage.src = subImage[3].src;
+};
